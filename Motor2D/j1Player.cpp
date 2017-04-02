702eb8a99369546abbb18d5f38d6cdea59f38a26
elliotjb <elliot.jimenez1@gmail.com>
2017-04-02 23:10:09 +0200

    Add hurt with enemies

--- Motor2D/j1Player.cpp
+++ Motor2D/j1Player.cpp
@@ -277,6 +277,23 @@ void Player::OnCollision(Collider* c1, Collider* c2)
 			}
 		}
 
+		if (c1 == collision_feet && c2->type == COLLIDER_ENEMY)
+		{
+			if (hurt == false)
+			{
+				timer.Start();
+				hurt = true;
+				hp_hearts.y--;
+			}
+			else
+			{
+				if (timer.ReadSec() >= 1)
+				{
+					hurt = false;
+				}
+			}
+		}
+
 		if (c1 == collision_attack && c2->type == COLLIDER_ENEMY)
 		{
 			Soldier* soldier = (Soldier*)c2->callback;
