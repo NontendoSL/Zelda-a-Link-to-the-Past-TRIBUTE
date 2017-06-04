6d71babaf88f0276a577606b719df098c4032094
xDragan <marcfv97@gmail.com>
2017-06-05 01:21:23 +0200

    Video can be skipped with controller

--- Motor2D/Video.cpp
+++ Motor2D/Video.cpp
@@ -4,6 +4,7 @@
 #include "p2Log.h"
 #include "j1Window.h"
 #include "j1Input.h"
+#include "j1InputManager.h"
 #include "j1Textures.h"
 #include "j1Render.h"
 #include "j1Audio.h"
@@ -199,7 +200,7 @@ bool Video::PostUpdate()
 
 		// Events -------------------------------------------------------
 
-		if (App->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		if (App->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN || App->input_manager->EventPressed(INPUTEVENT::BUTTON_START) == EVENTSTATE::E_DOWN)
 			quit = 1;
 
 		// ----------------------------------------------------------------
